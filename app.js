4724bf2886ad2834ffab0e01a7c94361d0d6214f
dworker <dworker863@gmail.com>
2021-08-03 17:16:55 +0600

    File Upload

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ const usersRouter = require('./routes/users');
 const dishRouter = require('./routes/dishRouter');
 const promoRouter = require('./routes/promoRouter');
 const leaderRouter = require('./routes/leaderRouter');
+const uploadRouter = require('./routes/uploadRouter');
 
 mongoose.Promise = global.Promise;
 const url = config.mongoUrl;
@@ -64,6 +65,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/dishes', dishRouter);
 app.use('/promotions', promoRouter);
 app.use('/leaders', leaderRouter);
+app.use('/imageUpload', uploadRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
